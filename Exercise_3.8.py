e18c63dc6c034e6e0141a7bbf2c9e6bb24bf206f
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-11-14 12:32:51 +0100

    Exercise_3.8.py

--- Exercise_3.8.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Please type which time table you want to see :"))
-if number < 0:
-    for n in range(12, 0, -1):
-        print(n, "*", number, "equals", n * number)
-for n in range(1, 13):
-    print(n, "*", number, "equals", n * number)
